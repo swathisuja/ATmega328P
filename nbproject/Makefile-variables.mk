60fb029ad8f18b7c0121b45bf679a32b9d9bf096
swathi.moorthy@microchip.com <swathi.moorthy@microchip.com>
2021-03-25 14:56:49 +0000

    Project created

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=328p.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/328p.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=328p.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/328p.tar
